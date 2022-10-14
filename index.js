1936bfafb4b1bb28d986c9b493dec6b7d3792dca
Ralf Bonnick <ralfbonnick@gmx.net>
2022-10-14 16:53:05 +0200

    gamestate 2 start with JS

---
+++ index.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d');
+
+const gameState1 = document.querySelector("#gameState1");
+const gameState2 = document.querySelector("#gameState2");
+const gameState3 = document.querySelector("#gameState3");
+
+// background
+const background = new Image();
+background.src = "../images/Background01.png"
+
+
+function startGame() {
+    gameState1.style.display = "none";
+    gameState2.style.display = "block";
+    // animate()
+  };
+
+
+  window.onload = () => {
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    document.getElementById('playBtn').onclick = () => {
+    startGame();
+    };
+  
+  };
+
+
+
