063e740972f3f3592f34e8b1f49740448b91c40d
Ralf Bonnick <ralfbonnick@gmx.net>
2022-10-14 17:45:24 +0200

    gamestate 2 add ship

--- index.js
+++ index.js
@@ -9,21 +9,58 @@ const gameState3 = document.querySelector("#gameState3");
 const background = new Image();
 background.src = "../images/Background01.png"
 
+// gamer
+const ship = new Image();
+ship.src = "../images/Spaceship.png"
+let shipWidth = 100;
+let shipHeight = 110;
+let shipX = canvas.width / 2;
+let shipY = canvas.height - shipHeight;
+let isShipGoingLeft = false;
+let shipSpeed = 1.5;
 
+let animationFrameId = 0;
+let gameOver = false;
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(ship, shipX, shipY, shipWidth, shipHeight);
+    if (isShipGoingLeft) {
+        if (shipX > 0) {
+            shipX -=  shipSpeed;
+        }
+      };
+
+      if (gameOver) {
+        cancelAnimationFrame(animationFrameId);
+      } else {
+        animationFrameId = requestAnimationFrame(animate);
+      }
+}
 
 function startGame() {
     gameState1.style.display = "none";
     gameState2.style.display = "block";
-    //animate();
+    animate();
   };
 
 
   window.onload = () => {
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     document.getElementById('playBtn').onclick = () => {
     startGame();
     };
-  
+    document.addEventListener("keydown", event => {
+        if (event.code === "ArrowLeft") {
+            isShipGoingLeft = true;
+            console.log(isShipGoingLeft);
+        }
+        
+      });
+      
+      document.addEventListener("keyup", event => {
+        isShipGoingLeft = false;
+      });
   };
 
 
